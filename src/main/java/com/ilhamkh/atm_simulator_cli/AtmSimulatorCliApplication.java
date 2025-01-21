757a3dbac1151a2ea4353afd4776ec4f0c65a529
ilhamkhairil <khairil.ilham.net@gmail.com>
2025-01-21 16:39:01 +0700

    Initial commit

---
+++ src/main/java/com/ilhamkh/atm_simulator_cli/AtmSimulatorCliApplication.java
@@ -0,0 +1,13 @@
+package com.ilhamkh.atm_simulator_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSimulatorCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSimulatorCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilhamkh/atm_simulator_cli/AtmSimulatorCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilhamkh.atm_simulator_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulatorCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
