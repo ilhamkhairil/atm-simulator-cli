1841dbf1faa11285077cb9650191074fd2e79b6e
ilhamkhairil <khairil.ilham.net@gmail.com>
2025-01-21 19:48:15 +0700

    adding scenario test

--- src/main/java/com/ilhamkh/atm_simulator_cli/AtmSimulatorCliApplication.java
+++ src/main/java/com/ilhamkh/atm_simulator_cli/AtmSimulatorCliApplication.java
@@ -1,13 +1,26 @@
 package com.ilhamkh.atm_simulator_cli;
 
+
+import com.ilhamkh.atm_simulator_cli.controller.AtmController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class AtmSimulatorCliApplication {
+public class AtmSimulatorCliApplication implements CommandLineRunner {
+
+	@Autowired
+	AtmController controller;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmSimulatorCliApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		controller.doProcess(System.in, System.out);
+	}
 }
